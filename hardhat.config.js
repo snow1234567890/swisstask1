34743dffa95ca826554abaf2b85f673382946157
carzolarjiang <carzolarjiang@gmail.com>
2024-07-28 11:20:01 +0000

    drop privatekey

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0d995b693d1902256f8f645f747365b21d73d3e9332b3f360009ee240b0e8421"], //Your private key starting with "0x"
+      accounts: [""], //Your private key starting with "0x"
     },
   },
 };
